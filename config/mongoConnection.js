923c211648eadc5c753de6d2f84d727ce91f1627
Arceus1ooo <56331076+Arceus1ooo@users.noreply.github.com>
2022-04-18 21:41:19 -0400

    setup mongo config

---
+++ config/mongoConnection.js
@@ -0,0 +1,26 @@
+const MongoClient = require('mongodb').MongoClient;
+const settings = {
+    mongoConfig: {
+        serverUrl: "mongodb://localhost:27017/",
+        database: "Stevens_Marketplace"
+    }
+}
+const mongoConfig = settings.mongoConfig;
+
+let _connection = undefined;
+let _db = undefined;
+
+module.exports = {
+    async dbConnection() {
+        if (!_connection) {
+            _connection = await MongoClient.connect(mongoConfig.serverUrl, {
+                useNewUrlParser: true
+            });
+            _db = await _connection.db(mongoConfig.database);
+        }
+        return _db;
+    },
+    async closeConnection() {
+        _connection.close();
+    }
+};
