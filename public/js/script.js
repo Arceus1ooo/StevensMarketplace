83ecafd9e3132fb94b52427aaf0437290c771f47
jnelson6 <jnelson6@stevens.edu>
2022-05-08 08:19:55 -0400

    converted all pages to handlebar layouts and partials

---
+++ public/js/script.js
@@ -0,0 +1,15 @@
+const handleLogin = () => {
+  //handle login
+};
+
+const handleLogout = () => {
+  //handle logout
+};
+
+const handleSearch = () => {
+  //handle search
+};
+
+const handleFilters = () => {
+  //handle filters
+};
