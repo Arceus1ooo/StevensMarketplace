016f9cac94a070fdd876e6f652a9003c8295e536
Arceus1ooo <56331076+Arceus1ooo@users.noreply.github.com>
2022-05-09 00:00:42 -0400

    finishing touches

--- data/index.js
+++ data/index.js
@@ -3,49 +3,6 @@ const listingsData = require('./listings');
 const reviewsData = require('./reviews');
 const conversationsData = require('./conversations');
 
-async function test() {
-    try {
-        let test = await usersData.createUser('test@test.com', 'password');
-        console.log(test);
-    } catch (e) {
-        console.log(e);
-    }
-    try {
-        let test = await usersData.createUser('blah2@blah.com', 'password1');
-        console.log(test);
-    } catch (e) {
-        console.log(e);
-    }
-    let user1 = await usersData.getUserByEmail('test@test.com');
-    let user2 = await usersData.getUserByEmail('blah2@blah.com');
-    //await usersData.deleteUserByEmail('teSt@teSt.Com');
-    let listing = await listingsData.createListing('test', 'furniture', '3/3/2003', '23.21', 'test', 'good', true, user1._id);
-    //await listingsData.deleteListingByID(listing._id);
-    let review = await reviewsData.createReview(user1._id, user2._id, listing._id, 'test', 3.9);
-    try {
-        let review2 = await reviewsData.createReview(user1._id, user2._id, listing._id, 'test', 3.9);
-    }
-    catch (e) {
-        console.log(e);
-    }
-    await reviewsData.updateReview(user2._id, listing._id, 'new test', 3.1);
-    let listing2 = await listingsData.createListing('test', 'textbook', '3/3/2003', '23.21', 'test', 'good', true, user1._id);
-    //await listingsData.deleteListingByID(listing2._id);
-    let a = await listingsData.getListingsByCategory('furniture');
-    console.log(a);
-
-    try {
-        let conversation = await conversationsData.createConversation(user2._id, user1._id);
-    } catch (e) {
-        console.log(e);
-    }
-    let message = await conversationsData.createMessage(user1._id, 'hello');
-    console.log(message);
-    await conversationsData.addMessage(message, user2._id, user1._id);
-    console.log(await conversationsData.getAllConversations(user1._id));
-    console.log('done');
-}
-//test();
 
 module.exports = {
     users: usersData,
--- tasks/seed.js
+++ tasks/seed.js
@@ -1,10 +1,10 @@
 const bcrypt = require('bcryptjs');
 const dbConnection = require('../config/mongoConnection');
 const data = require('../data');
-const users = data.users;
-const reviews = data.reviews;
-const listings = data.listings;
-const conversations = data.conversations;
+const usersData = data.users;
+const reviewsData = data.reviews;
+const listingsData = data.listings;
+const conversationsData = data.conversations;
 
 // Generate hashedPasswords for seed users, using 10 salt rounds for efficiency
 const genHP = (plainTextPW) => {
@@ -12,65 +12,46 @@ const genHP = (plainTextPW) => {
 }
 
 const main = async () => {
-    const db = await dbConnection();
-    await db.dropDatabase();
-
-    // Now we create 5 student users
-    let user1;
     try {
-        user1 = await users.createUser('test@test.com', 'password');
+        let test = await usersData.createUser('test@test.com', 'password');
+        console.log(test);
     } catch (e) {
-        console.log(`Error in creation of user 1: ${e}`)
+        console.log(e);
     }
-    let user2;
     try {
-        user2 = await users.createUser('blah2@blah.com', 'password1');
+        let test = await usersData.createUser('blah2@blah.com', 'password1');
+        console.log(test);
     } catch (e) {
-        console.log(`Error in creation of user 2: ${e}`)
+        console.log(e);
     }
-    let user3;
+    let user1 = await usersData.getUserByEmail('test@test.com');
+    let user2 = await usersData.getUserByEmail('blah2@blah.com');
+    //await usersData.deleteUserByEmail('teSt@teSt.Com');
+    let listing = await listingsData.createListing('test', 'furniture', '3/3/2003', '23.21', 'test', 'good', true, user1._id);
+    //await listingsData.deleteListingByID(listing._id);
+    let review = await reviewsData.createReview(user1._id, user2._id, listing._id, 'test', 3.9);
     try {
-        user3 = await users.createUser('yoyo@gmail.com', 'password2');
-    } catch (e) {
-        console.log(`Error in creation of user 3: ${e}`)
+        let review2 = await reviewsData.createReview(user1._id, user2._id, listing._id, 'test', 3.9);
     }
-
-    let listings1;
-    try {
-        listings1 = await listings.createListing('test', 'furniture', '3/3/2003', '23.21', 'test', 'good', true, user1._id);
-    } catch (e) {
-        console.log(`Error in creation of listing 1: ${e}`);
+    catch (e) {
+        console.log(e);
     }
-
-    let reviews1;
-    try {
-        reviews1 = await reviews.createReview(user1._id, user2._id, listing._id, 'test', 3.9);
-    } catch (e) {
-        console.log(`Error in creation of review 1: ${e}`);
-    }
-
-    let conversations1;
-    try {
-        conversations1 = await conversations.createConversation(user3._id, user1._id)
-    } catch (e) {
-        console.log(`Error in creation of conversation 1: ${e}`);
-    }
-
-    let message1;
-    try {
-        message1 = await conversations.createMessage(user1._id, 'hello');
-    } catch (e) {
-        console.log(`Error in creation of message 1: ${e}`);
-    }
+    await reviewsData.updateReview(user2._id, listing._id, 'new test', 3.1);
+    let listing2 = await listingsData.createListing('test', 'textbook', '3/3/2003', '23.21', 'test', 'good', true, user1._id);
+    //await listingsData.deleteListingByID(listing2._id);
+    let a = await listingsData.getListingsByCategory('furniture');
+    console.log(a);
 
     try {
-        await conversations.addMessage(message1, user1._id, user2._id);
+        let conversation = await conversationsData.createConversation(user2._id, user1._id);
     } catch (e) {
-        console.log(`Error in adding message 1: ${e}`);
+        console.log(e);
     }
-
-    console.log('\nFinished seeding database!');
-    await db.serverConfig.close();
+    let message = await conversationsData.createMessage(user1._id, 'hello');
+    console.log(message);
+    await conversationsData.addMessage(message, user2._id, user1._id);
+    console.log(await conversationsData.getAllConversations(user1._id));
+    console.log('done');
 }
 
 main();
