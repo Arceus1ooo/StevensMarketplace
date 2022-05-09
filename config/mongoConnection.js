4970cb3b3d26cc73aa8941d9ecc5004de2591d3e
Sebastian Muriel <smuriel@stevens.edu>
2022-05-08 23:59:00 -0400

    Tweaks part 3

--- config/mongoConnection.js
+++ config/mongoConnection.js
@@ -10,14 +10,17 @@ const mongoConfig = settings.mongoConfig;
 let _connection = undefined;
 let _db = undefined;
 
-module.exports = async () => {
-    if (!_connection) {
-        _connection = await MongoClient.connect(mongoConfig.serverUrl, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        _db = await _connection.db(mongoConfig.database);
+module.exports = {
+    async dbConnection() {
+        if (!_connection) {
+            _connection = await MongoClient.connect(mongoConfig.serverUrl, {
+                useNewUrlParser: true
+            });
+            _db = await _connection.db(mongoConfig.database);
+        }
+        return _db;
+    },
+    async closeConnection() {
+        _connection.close();
     }
-
-    return _db;
 };
