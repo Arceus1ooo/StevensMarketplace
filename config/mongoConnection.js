152c0035307b444ef77838894fcf362edaa7ca8a
Sebastian Muriel <smuriel@stevens.edu>
2022-05-08 23:49:05 -0400

    Tweaks

--- config/mongoConnection.js
+++ config/mongoConnection.js
@@ -10,17 +10,14 @@ const mongoConfig = settings.mongoConfig;
 let _connection = undefined;
 let _db = undefined;
 
-module.exports = {
-    async dbConnection() {
-        if (!_connection) {
-            _connection = await MongoClient.connect(mongoConfig.serverUrl, {
-                useNewUrlParser: true
-            });
-            _db = await _connection.db(mongoConfig.database);
-        }
-        return _db;
-    },
-    async closeConnection() {
-        _connection.close();
+module.exports = async () => {
+    if (!_connection) {
+        _connection = await MongoClient.connect(mongoConfig.serverUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        _db = await _connection.db(mongoConfig.database);
     }
+
+    return _db;
 };
